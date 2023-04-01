b484e77e028f4b31a20ce4342d3d10141130a6ad
Чехонина Екатерина <katerinasheps@gmail.com>
2023-04-01 18:11:03 +0300

    Добавила кнопку переключения языка: пока что не работает динамическая смена

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template, request
 from googletrans import Translator
 import re
 import requests
+from flask_wtf import Form
+from wtforms import SelectField
+
 
 app = Flask(__name__)
 
+class Buttons(Form):
+    language = SelectField('Language', choices=[('en', 'English'), ('ru', 'Russian')], default='en')
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    buttons = Buttons()
+
     if request.method == 'POST':
         test_text = request.form['test_text']
         user_text = request.form['user_text']
@@ -15,14 +23,17 @@ def home():
     
     words_count = 100
     language = 'en'
+    if buttons.validate():
+        language = str(buttons.language.data)
     punctuation = True #False
 
     url = "https://fish-text.ru/get?&number=8"
     response = requests.get(url)
 
     if response.status_code == 200:
+        
+        
         data = response.json()['text']
-
         if (language == 'en'):
             translator = Translator()
             data = translator.translate(data, dest='en')
@@ -32,8 +43,9 @@ def home():
             data = re.sub(r'[^\w\s]', '', data) 
         
         test_text = " ".join(data.split()[:words_count])
+        
 
-    return render_template('home.html', test_text=test_text)
+    return render_template('home.html', test_text=test_text, buttons=buttons)
 
 def calculate_result(test_text, user_text):
     test_words = test_text.split()
