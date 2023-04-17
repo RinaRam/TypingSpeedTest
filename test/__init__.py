56d85c9fcfeeb4a95df297a53e7d68c182fe5bbd
Чехонина Екатерина <katerinasheps@gmail.com>
2023-04-17 21:04:14 +0300

    Покрыла тестами функциб generate_text

--- test/__init__.py
+++ test/__init__.py
@@ -12,11 +12,36 @@ sys.path.remove(os.path.abspath(os.path.join(testdir, srcdir)))
 
 punct = re.compile(r"[^\w\s]")
 ru = re.compile(r"[^A-Яа-я\s]")
+en = re.compile(r"[^A-Za-z\s]")
 
 class TestGenTest(unittest.TestCase):
 
-    def test_0_ru_10_false(self):
+    def test_0_ru(self):
+        text = generate_text("ru", 10, False)
+        self.assertFalse(re.findall(ru, text))
+
+    def test_1_en(self):
+        text = generate_text("en", 10, False)
+        self.assertFalse(re.findall(en, text))
+
+    def test_2_10(self):
         text = generate_text("ru", 10, False)
         self.assertEqual(len(text.split()), 10)
+
+    def test_3_40(self):
+        text = generate_text("ru", 40, False)
+        self.assertEqual(len(text.split()), 40)
+
+
+    def test_4_100(self):
+        text = generate_text("ru", 100, False)
+        self.assertEqual(len(text.split()), 100)
+
+    
+    def test_5_false(self):
+        text = generate_text("ru", 10, False)
         self.assertFalse(re.findall(punct, text))
-        self.assertFalse(re.findall(ru, text))
+
+    def test_6_true(self):
+        text = generate_text("ru", 10, True)
+        self.assertTrue(re.findall(punct, text))
