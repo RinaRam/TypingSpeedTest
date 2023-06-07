c159fd69b5484614242e4210c21a0da6e782db64
Ramina Mudarova <animarosaceus@mail.ru>
2023-06-08 01:22:48 +0300

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -6,23 +6,37 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Typing Speed Test'
-copyright = '2023, Мударова Рамина, Чехонина Екатерина'
-author = 'Мударова Рамина, Чехонина Екатерина'
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TypingSpeedTest'
+copyright = '2023, Chekhonina Ekaterina, Mudarova Ramina'
+author = 'Chekhonina Ekaterina, Mudarova Ramina'
 release = '0.1'
+
+language = "English"
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'ru'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'yummy_sphinx_theme'
 html_static_path = ['_static']
