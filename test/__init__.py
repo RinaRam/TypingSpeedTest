28b3c5675b04e9e67f12a02c1ff38771538593a5
Чехонина Екатерина <katerinasheps@gmail.com>
2023-06-07 20:28:25 +0300

    Добавила тесты на calculate_cwpm

--- test/__init__.py
+++ test/__init__.py
@@ -7,7 +7,7 @@ testdir = os.path.dirname(__file__)
 srcdir = '../app'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
-from tools import generate_text, calculate_result
+from tools import generate_text, calculate_result, calculate_cwpm
 sys.path.remove(os.path.abspath(os.path.join(testdir, srcdir)))
 
 punct = re.compile(r"[^\w\s]")
@@ -83,3 +83,54 @@ class TestCalcResult(unittest.TestCase):
         user_text = self.test_text + "extra word"
         res = calculate_result(self.test_text, user_text)
         self.assertEqual(res, 0.8)
+
+
+
+class TestCalcCWPM(unittest.TestCase):
+
+    def setUp(self):
+        self.test_text = '''
+            King added in a furious passion and went back to
+        '''
+
+    def test_0_0(self):
+        user_text = ""
+        res = calculate_cwpm(self.test_text, user_text, 10)
+        self.assertEqual(res, 0)
+
+    def test_1_some_val_10(self):
+        user_text = "King added in a furious"
+        res = calculate_cwpm(self.test_text, user_text, 10)
+        self.assertEqual(res, 30)
+
+    def test_1_some_val_30(self):
+        user_text = "King added in a furious"
+        res = calculate_cwpm(self.test_text, user_text, 30)
+        self.assertEqual(res, 10)
+
+    def test_1_some_val_60(self):
+        user_text = "King added in a furious"
+        res = calculate_cwpm(self.test_text, user_text, 60)
+        self.assertEqual(res, 5)
+
+    def test_2_wrong_case(self):
+        user_text = "king added in a furious"
+        res = calculate_cwpm(self.test_text, user_text, 5)
+        self.assertEqual(res, 48)
+
+    def test_3_some_mistakes(self):
+        user_text = "king adde In a furious and went back to"
+        res = calculate_cwpm(self.test_text, user_text, 20)
+        self.assertEqual(res, 6)
+
+
+    def test_4_perfect(self):
+        user_text = self.test_text
+        res = calculate_cwpm(self.test_text, user_text, 60)
+        self.assertEqual(res, 10)
+
+    
+    def test_5_too_much(self):
+        user_text = self.test_text + "extra word"
+        res = calculate_cwpm(self.test_text, user_text, 30)
+        self.assertEqual(res, 16)
