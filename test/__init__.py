9e318cfcdbb5cf98e9655955ad37d3ea30d6b823
Ramina Mudarova <animarosaceus@mail.ru>
2023-06-08 02:06:54 +0300

    Синхронизация

--- test/__init__.py
+++ test/__init__.py
@@ -14,6 +14,38 @@ punct = re.compile(r"[^\w\s]")
 ru = re.compile(r"[^A-Яа-я\s]")
 en = re.compile(r"[^A-Za-z\s]")
 
+class TestGenTest(unittest.TestCase):
+    """Класс тестов генерации текста"""
+
+    def test_0_ru(self):
+        text = generate_text("ru", 10, False)
+        self.assertFalse(re.findall(ru, text))
+
+    def test_1_en(self):
+        text = generate_text("en", 10, False)
+        self.assertFalse(re.findall(en, text))
+
+    def test_2_10(self):
+        text = generate_text("ru", 10, False)
+        self.assertEqual(len(text.split()), 10)
+
+    def test_3_40(self):
+        text = generate_text("ru", 40, False)
+        self.assertEqual(len(text.split()), 40)
+
+    def test_4_100(self):
+        text = generate_text("ru", 100, False)
+        self.assertEqual(len(text.split()), 100)
+    
+    def test_5_false(self):
+        text = generate_text("ru", 10, False)
+        self.assertFalse(re.findall(punct, text))
+
+    def test_6_true(self):
+        text = generate_text("ru", 10, True)
+        self.assertTrue(re.findall(punct, text))
+
+        
 class TestCalcResult(unittest.TestCase):
     """Класс тестов для страницы /home"""
 
